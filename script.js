b5c3f8104566d97a110c6bce91bf6ea98bbfd54f
MohammadKaish <176997248+MohammadKaish@users.noreply.github.com>
2025-01-25 13:38:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function time(){
+    let newtime = document.querySelector("#time");
+    let time = new Date()
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+    let seconds = time.getSeconds()
+    newtime.innerText = `${hours}:${minutes}:${seconds}`;
+    
+
+}
+// time(setInterval(time,1000))
+setInterval(time,1000)
+console.log(time)
